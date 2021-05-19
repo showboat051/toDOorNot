c9d2cb86e49e9366409ddc2b292131254e6fdb59
showboat051 <rashad.davis87@yahoo.com>
2021-05-19 02:45:48 -0500

    populates to do

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const taskButton = document.getElementById("taskButton");
+const taskInput = document.getElementById("taskInput");
+const myList = document.getElementById("myList");
 
 
 
@@ -8,4 +10,19 @@ const taskButton = document.getElementById("taskButton");
 taskButton.addEventListener( "click", function (e) {
     e.preventDefault();
     console.log("clicked");
-}) 
+});
+
+function newToDo() {
+   var li = document.createElement("li");
+   var userInput = document.getElementById("taskInput").value;
+   var userTask = document.createTextNode(userInput);
+   li.appendChild(userTask);
+   if (userInput === '') {
+       alert("Don't be lazy, DO something!");
+   } else {
+        myList.appendChild(li);
+   }
+   taskInput.value = "";
+
+  
+}
