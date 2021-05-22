36fbb69c305f907266e9fe512bc8e7ddce3d5722
showboat051 <rashad.davis87@yahoo.com>
2021-05-22 01:20:20 -0500

    redoing code

--- script.js
+++ script.js
@@ -12,17 +12,59 @@ taskButton.addEventListener( "click", function (e) {
     console.log("clicked");
 });
 
-function newToDo() {
-   var li = document.createElement("li");
-   var userInput = document.getElementById("taskInput").value;
-   var userTask = document.createTextNode(userInput);
-   li.appendChild(userTask);
-   if (userInput === '') {
-       alert("Don't be lazy, DO something!");
-   } else {
-        myList.appendChild(li);
-   }
-   taskInput.value = "";
+
+
+
+// function newToDo() {
+//    var li = document.createElement("li");
+//    var userInput = document.getElementById("taskInput").value;
+//    var userTask = document.createTextNode(userInput);
+//    li.appendChild(userTask);
+
+
+// // Delete button clicks
+// var eraseTask = document.getElementsByClassName("erase");
+// // var i;
+// for (i = 0; i < eraseTask.length; i++) {
+//     eraseTask[i].onclick = function() {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//     }
+// } 
+
+// //Delete button for every list item
+// var myNewList = document.getElementsByTagName("LI");
+// // var i;
+// for (i = 0; i < myNewList.length; i++) {
+//     var span = document.createElement("SPAN");
+//     var x = document.createTextNode("\u00D7");
+//     span.className = "erase";
+//     span.appendChild(x);
+//     myNewList[i].appendChild(span);
+// }
+
+
+//    if (userInput === '') {
+//        alert("Don't be lazy, DO something!");
+//    } else {
+//         myList.appendChild(li);
+//    }
+//    taskInput.value = "";
 
   
+// } // end of newToDo()
+
+
+function postToDo() {
+   
+    
+    if(taskInput.value === '') {
+        alert("Don't be lazy, DO SOMETHING")
+    } else {
+        let newListItem = document.createElement('li');
+        let newToDo = document.createTextNode(taskInput.value);
+        newListItem.appendChild(newToDo);
+         
+
+    }
 }
